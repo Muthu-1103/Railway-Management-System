9668c53c0d34417bf7f7050910196274b25e708d
Muthu-1103 <129935573+Muthu-1103@users.noreply.github.com>
2024-03-24 20:16:23 +0530

    Add files via upload

---
+++ viewer.java
@@ -0,0 +1,19 @@
+
+import com.gnostice.pdfone.PdfDocument;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER LAPTOP
+ */
+class viewer {
+
+    static void loadDocument(PdfDocument d) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
